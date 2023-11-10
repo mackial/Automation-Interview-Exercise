7b523960f4343b4148ecddaa58bf2ab645987282
Marcial Amrbiz <jmaramgo@gmail.com>
2023-11-10 00:10:07 -0600

    Finish challenge

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Automation-Interview-Exercise
+
+---
+
+## Running Locally
+
+Make sure to assign the corresponding values to the environment variables.
+
+```sh
+npm install
+npm run dev 
+```
+
+## Environment Variables
+
+1. Create .env file in project root directory
+2. Copy example.env contents to .env file
+3. Setup email and password todoist values for login
--- index.js
+++ index.js
@@ -7,21 +7,33 @@ import 'dotenv/config';
     headless: false,
   });
 
-  // // Trello tasks
-  // const trello = await browser.newPage();
+  // Trello
+  const trello = await browser.newPage();
 
-  // await trello.goto('https://trello.com/b/QvHVksDa/personal-work-goals');
+  await trello.goto('https://trello.com/b/QvHVksDa/personal-work-goals');
 
-  // // Set screen size
-  // await trello.setViewport({width: 1080, height: 1024});
+  // Set screen size
+  await trello.setViewport({ width: 1080, height: 1024 });
 
-  // Todoist Login
+  // Close about this board
+  const aboutModalButton = '[data-testid="about-this-board-modal-cta-button"]';
+  await trello.waitForSelector(aboutModalButton);
+  await trello.click(aboutModalButton);
+
+  const taskList = await trello.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('[data-testid="card-name"]'),
+      (element) => element.textContent
+    )
+  );
+
+  // Todoist
   const todoist = await browser.newPage();
 
   await todoist.goto('https://app.todoist.com/auth/login');
 
   // Set screen size
-  await todoist.setViewport({width: 1080, height: 1024});
+  await todoist.setViewport({ width: 1080, height: 1024 });
 
   // Login inputs
   const tdstEmailInput = '#element-0';
@@ -38,14 +50,54 @@ import 'dotenv/config';
   await todoist.waitForSelector(tdstLoginButton);
   await todoist.click(tdstLoginButton);
 
-  // // Locate the full title with a unique string
-  // const textSelector = await page.waitForSelector(
-  //   ' '
-  // );
-  // const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  const firstTenTasks = taskList.splice(0, 10);
 
-  // // Print the full title
-  // console.log('The title of this blog post is "%s".', fullTitle);
+  // Add task
+  await firstTenTasks.map(async (t, i) => {
+    await new Promise((resolve) => {
+      setTimeout(async () => {
+        console.log('interval1', t);
+        const resolved = await todoist.evaluate(async (tt) => {
+          return await new Promise((resolve) => {
+            const interval2 = setInterval(() => {
+              const newTask = document.getElementById('quick_add_task_holder');
+
+              if (newTask) {
+                newTask.click();
+
+                // Wait to render task editor
+                const interval3 = setInterval(() => {
+                  const taskNameHolder = document.querySelector(
+                    '[aria-label="Task name"]'
+                  );
+                  // Set the task name
+                  taskNameHolder.innerHTML = `<p>${tt}</p>`;
+
+                  if (taskNameHolder) {
+                    // Wait to enable add task button
+                    setTimeout(() => {
+                      const addTask = document.querySelector(
+                        '[data-testid="task-editor-submit-button"]'
+                      );
+                      addTask.click();
+
+                      // Clear intervals
+                      clearInterval(interval3);
+                      clearInterval(interval2);
+                    }, 300);
+                  }
+                }, 300);
+              }
+            }, 1000);
+
+            resolve(true);
+          });
+        }, t);
+
+        if (resolved) resolve();
+      }, 1400 * (i + 1));
+    });
+  });
 
   // await browser.close();
 })();
